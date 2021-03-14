14b40f08b7f42c94002896dbcce01e202592f040
Wesley Moore <wes@wezm.net>
2021-03-14 20:04:52 +1000

    Add Cirrus CI config

--- spec/setup/configure_lucky_flow.cr
+++ spec/setup/configure_lucky_flow.cr
@@ -1,5 +1,8 @@
 LuckyFlow.configure do |settings|
   settings.stop_retrying_after = 200.milliseconds
   settings.base_uri = Lucky::RouteHelper.settings.base_uri
+  if ENV["LUCKYFLOW_DRIVER_PATH"]? # Used in CI
+    settings.driver_path = ENV["LUCKYFLOW_DRIVER_PATH"]
+  end
 end
 Spec.before_each { LuckyFlow::Server::INSTANCE.reset }
